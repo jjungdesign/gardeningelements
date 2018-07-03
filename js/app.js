928b9f861c275548f09e7b51943a8c3d8873ed87
jjungdesign <31354959+jjungdesign@users.noreply.github.com>
2018-07-03 10:55:12 -0400

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -55,9 +55,14 @@ $(document).ready(() => {
   var span = document.getElementsByClassName("close")[0];
 
   // When the user clicks on the button, open the modal
-  btn.onclick = function() {
-      modal.style.display = "block";
-  }
+  // btn.onclick = function() {
+  //     modal.style.display = "block";
+  // }
+
+  $("#aboutLink").click(function() {
+    $("#myModal").show();
+  });
+
 
   // When the user clicks on <span> (x), close the modal
   span.onclick = function() {
